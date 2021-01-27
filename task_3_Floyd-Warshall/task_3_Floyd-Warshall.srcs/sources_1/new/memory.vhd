25941c013189659f0ceaa6b8e5df44620db80d15
Aleksey Tyurin <alekseytyurinspb@gmail.com>
2021-01-27 16:28:02 +0300

    non-bramable memory

--- task_3_Floyd-Warshall/task_3_Floyd-Warshall.srcs/sources_1/new/memory.vhd
+++ task_3_Floyd-Warshall/task_3_Floyd-Warshall.srcs/sources_1/new/memory.vhd
@@ -21,26 +21,41 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity bram is
-    generic(NUMBER_OF_VERTICES : integer := 64);
+    generic(NUMBER_OF_VERTICES : integer range 1 to 64 := 64; NUM_SIZE : integer := 32);
     port ( addr : in integer range 0 to NUMBER_OF_VERTICES - 1;
+           scalar_read_e : in std_logic;
+
+           scalar_column : in integer range 0 to NUMBER_OF_VERTICES - 1;
            clk : in std_logic;
-           d_input : in std_logic_vector(0 to (NUMBER_OF_VERTICES * 32 ) - 1);
+           d_input : in std_logic_vector(0 to (NUMBER_OF_VERTICES * NUM_SIZE ) - 1);
            we : in std_logic;
-           d_output : out std_logic_vector(0 to (NUMBER_OF_VERTICES * 32) - 1)
+           d_output : out std_logic_vector(0 to (NUMBER_OF_VERTICES * NUM_SIZE) - 1);
+           d_scalar_output : out std_logic_vector(0 to NUM_SIZE - 1)
     ); 
 end bram;
 
 architecture syn of bram is
 --    type mem is array (ROWS - 1 downto 0) of tarr(COLUMNS - 1 downto 0);  it is sytesized down to lots of LUT and FF instead of BRAM
-    type mem is array (0 to NUMBER_OF_VERTICES - 1) of std_logic_vector(0 to (NUMBER_OF_VERTICES * 32) - 1);
+    type mem is array (0 to NUMBER_OF_VERTICES - 1) of std_logic_vector(0 to (NUMBER_OF_VERTICES * NUM_SIZE) - 1);
     signal memory : mem;
 begin
-    process(clk) begin
+    process(clk) 
+    
+    begin
+    
+    
     
     if rising_edge(clk) then
         if we = '1' then
             memory(addr) <= d_input;
-        else d_output <= memory(addr);
+        else 
+           if scalar_read_e = '1' then
+           
+                d_scalar_output <= memory(addr)(scalar_column * NUM_SIZE to (scalar_column + 1) * NUM_SIZE - 1 );
+           
+           else
+            d_output <= memory(addr);
+           end if; 
         end if;
     end if;
     
